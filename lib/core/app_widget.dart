19feb34783352708ed70d762252aab65867cc9f8
Zé William <josewilliam@justto.com.br>
2021-04-21 00:31:17 -0300

    NLW5 🚧 Usa tela de Splash.

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,4 +1,4 @@
-import 'package:DevQuiz/home/home_page.dart';
+// import 'package:DevQuiz/home/home_page.dart';
 import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +7,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: SplashPage(),
     );
   }
 }
